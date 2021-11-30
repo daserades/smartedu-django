72d14c341fa7e26ca9cd55cad4efb7df5888b508
yhertekin <yhertekin@gmail.com>
2021-12-01 00:34:42 +0300

    added courses models

--- smartedu_con/smartedu/settings.py
+++ smartedu_con/smartedu/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "pages.apps.PagesConfig",
+    "courses.apps.CoursesConfig"
 ]
 
 MIDDLEWARE = [
@@ -123,7 +124,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
