9db5cdd49ca94f53bebbf02c5bf748d19e062fca
yhertekin <yhertekin@gmail.com>
2021-12-01 23:07:07 +0300

    added course page and view

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -5,3 +5,8 @@ def course_list(request):
   courses = Course.objects.all().order_by("-date")
   context = {"courses": courses}
   return render(request, "courses.html", context)
+
+def course_detail(request, category_slug, course_id):
+  course = Course.objects.get(category__slug=category_slug, id=course_id)
+  context = {"course": course}
+  return render(request, "course.html", context)
