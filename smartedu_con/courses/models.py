367145b9c04b3397da3e9c0ef45770466715a391
yhertekin <yhertekin@gmail.com>
2021-12-02 17:19:39 +0300

    added tag model

--- smartedu_con/courses/models.py
+++ smartedu_con/courses/models.py
@@ -11,11 +11,25 @@ class Category(models.Model):
     verbose_name = "Category"
 
   def __str__(self):
+    return self.name  
+  
+
+class Tag(models.Model):
+  name = models.CharField(max_length=50, null=True)
+  slug = models.SlugField(max_length=50, unique=True, null=True)
+
+  class Meta:
+    verbose_name_plural = "Tags"
+    verbose_name = "Tag"
+
+  def __str__(self):
     return self.name
+
 
 class Course(models.Model):
   name = models.CharField(max_length=200, unique=True)
   category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
+  tags = models.ManyToManyField(Tag, null=True, blank=True)
   description = models.TextField(max_length=500, blank=True, null=True)
   image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_image.jpg")
   date = models.DateTimeField(auto_now=True)
